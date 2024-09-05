ea2fe62ab9081934e20e70aa61a14fd8f0c41fd6
mansinighot93 <mansinighot93@gmail.com>
2024-09-05 14:46:39 +0530

    changes

---
+++ Practice/basic_javascript.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20;
+
+function minMax()
+{
+    if(a > b)
+    {
+        console.log("a is grater than b");
+    }
+    else
+    {
+       console.log("b is grater than a");
+    }
+}
---
+++ Practice/loginform.js
@@ -0,0 +1,15 @@
+function Submit() 
+{
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username === "" || password === "") 
+    {
+        document.getElementById("error-message").innerHTML = "Both fields are required.";
+    } 
+    else 
+    {
+        document.getElementById("error-message").innerHTML = "";
+        console.log("Username: " + username);
+        console.log("Password: " + password);
+    }
+}
