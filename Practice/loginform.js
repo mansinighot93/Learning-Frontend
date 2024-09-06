4ea278b4da3e4c2d5886c4c127c3e484786481c7
mansinighot93 <mansinighot93@gmail.com>
2024-09-06 15:45:09 +0530

    changes

--- Practice/loginform.js
+++ Practice/loginform.js
@@ -1,4 +1,4 @@
-function Submit() 
+/*function Submit() 
 {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -13,4 +13,22 @@ function Submit()
         console.log("Password: " + password);
     }
 }
+*/
+var loginform=[];
+var count=loginform.length;
+const onInsertButtonClick=()=>{
+    var txtUsername=document.getElementById("username");
+    var Username=txtUsername.value ;
 
+    var txtPassword=document.getElementById("password");
+    var Password=txtPassword.value ;
+};
+
+var Login={
+    id:loginform.length+1,
+    Username:username,
+    Password:password
+}
+
+loginform.push(Login);
+console.log(loginform);
